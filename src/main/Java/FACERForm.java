252e3ebf407604866d74270f3684bed199af3b22
MahaKamalCh <mahakamal@tintash.com>
2021-03-25 21:00:59 +0500

    added capability to copy selected code and paste to editor

--- src/main/Java/FACERForm.java
+++ src/main/Java/FACERForm.java
@@ -1,10 +1,19 @@
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectUtil;
 import com.intellij.ui.components.JBScrollPane;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class FACERForm {
@@ -40,43 +49,21 @@ public class FACERForm {
                 if (evt.getClickCount() == 2 && evt.getButton() == MouseEvent.BUTTON1) {
                     // Double-click detected
                     int index = list.getSelectedIndex();
-                    showMethodBody(list.getSelectedValue().toString(), "5:34 PM\tTextMate bundle load error: Bundle ruby can't be registered\n" +
+                    showMethodBody(list.getSelectedValue().toString(),"@NotNull\n" +
+                            "    private MouseAdapter getMethodsListMouseAdapter() {\n" +
+                            "        return new MouseAdapter() {\n" +
+                            "            public void mouseClicked(MouseEvent evt) {\n" +
+                            "                JList list = (JList)evt.getSource();\n" +
+                            "                if (evt.getClickCount() == 2 && evt.getButton() == MouseEvent.BUTTON1) {\n" +
+                            "                    // Double-click detected\n" +
+                            "                    int index = list.getSelectedIndex();\n" +
+                            "                    showMethodBody(list.getSelectedValue().toString(),\"public void testSomething(){}\");\n" +
                             "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle rust can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle scss can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle search-result can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle shaderlab can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle shellscript can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle sql can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle swift can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle typescript-basics can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle vb can't be registered\n + 5:34 PM\tTextMate bundle load error: Bundle ruby can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle rust can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle scss can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle search-result can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle shaderlab can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle shellscript can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle sql can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle swift can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle typescript-basics can't be registered\n" +
-                            "\n" +
-                            "5:34 PM\tTextMate bundle load error: Bundle vb can't be registered\n");
+                            "                }\n" +
+                            "            }\n" +
+                            "        };\n" +
+                            "    }");
+
                 }
             }
         };
@@ -107,17 +94,48 @@ public class FACERForm {
         JButton getRelatedMethodsButton = new JButton(AllIcons.Diff.MagicResolve);
         getRelatedMethodsButton.setBorder(BorderFactory.createEmptyBorder());
         getRelatedMethodsButton.setPreferredSize(new Dimension(20,20));
+        getRelatedMethodsButton.setToolTipText("Search related methods");
         getRelatedMethodsButton.addActionListener(evt -> {
             ArrayList relatedMethods = FACERSearchService.getInstance().getRelatedMethods(methodSignature);
             populateRelatedMethods(relatedMethods.toArray());
         });
 
+        JButton copyMethodBodyButton = new JButton(AllIcons.Actions.Copy);
+        copyMethodBodyButton.setBorder(BorderFactory.createEmptyBorder());
+        copyMethodBodyButton.setPreferredSize(new Dimension(20,20));
+        copyMethodBodyButton.setToolTipText("Add code to my file");
+        copyMethodBodyButton.addActionListener(evt -> {
+            final Project project = ProjectUtil.guessCurrentProject(mainPanel);
+            @Nullable Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+            Document document = editor.getDocument();
+
+            // Work off of the primary caret to get the selection info
+            Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
+            int start = primaryCaret.getSelectionStart();
+            int end = primaryCaret.getSelectionEnd();
+
+            // Replace the selection with a fixed string.
+            // Must do this document change in a write action context.
+            WriteCommandAction.runWriteCommandAction(project, () ->
+                    document.replaceString(start, end, methodBody)
+            );
+
+            // De-select the text range that was just replaced
+            primaryCaret.removeSelection();
+
+            // Copy to clipboard
+//            StringSelection stringSelection = new StringSelection(methodBody);
+//            Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
+//            clpbrd.setContents (stringSelection, null);
+        });
+
         JTextArea textArea = new JTextArea(methodBody);
-        textArea.setEditable(true);
+//        textArea.setEditable(true);
         JScrollPane scrollableTextArea = new JBScrollPane(textArea);
 
         JPanel optionsWrapper = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         optionsWrapper.add(getRelatedMethodsButton);
+        optionsWrapper.add(copyMethodBodyButton);
         optionsWrapper.setBackground(Color.white);
 
         tabPanel.add(optionsWrapper);
