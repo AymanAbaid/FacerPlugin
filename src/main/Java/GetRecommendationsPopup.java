1d6f237068fed12014190915a1be8daaa9de3e82
MahaKamalCh <mahakamal@tintash.com>
2021-04-04 00:09:04 +0500

    edited enabler popup options

--- src/main/Java/GetRecommendationsPopup.java
+++ src/main/Java/GetRecommendationsPopup.java
@@ -4,13 +4,12 @@ import com.intellij.ui.components.JBList;
 
 public class GetRecommendationsPopup {
 
-    static String queryPopupOptions[]= {"Get Recommendations"};
+    static String queryPopupOptions[]= {"Get FACER recommendations"};
 
     static void display(Editor editor, GetRecommendations getRecommendationsCallback){
         final JBList<String> list = new JBList<>(queryPopupOptions);
 
         JBPopupFactory.getInstance().createListPopupBuilder(list)
-                .setTitle("FACER Actions")
                 .setItemChoosenCallback(() -> getRecommendationsCallback.getRecommendationsForQuery(editor.getSelectionModel().getSelectedText()))
                 .createPopup()
                 .showInBestPositionFor(editor);
