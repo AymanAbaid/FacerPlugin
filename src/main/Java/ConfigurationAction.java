3fd1d070ed4b6e64ef5852177251a29e491633cf
MahaKamalCh <mahakamal@tintash.com>
2021-04-12 01:49:24 +0500

    added configuration action to top menu

--- src/main/Java/ConfigurationAction.java
+++ src/main/Java/ConfigurationAction.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
 
-public class ConfigurationAction {
+public class ConfigurationAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        new FACERConfigurationDialogWrapper().showAndGet();
+    }
 }
